201dfdf628fdc237e59ccc82fc3a0e34d28b9743
vlad <vlad.bintintan@gmail.com>
2024-10-09 22:06:57 +0300

    Fix NullPointerException

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TeleOp.java
@@ -5,10 +5,12 @@ import org.firstinspires.ftc.teamcode.RobotHardware;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Kickathon")
 public class TeleOp extends OpMode {
-    RobotHardware robot = new RobotHardware(hardwareMap);
+    RobotHardware robot;
 
     @Override
-    public void init() {}
+    public void init() {
+        robot = new RobotHardware(hardwareMap);
+    }
 
     public void lift() {
         throw new UnsupportedOperationException("Not implemented");
