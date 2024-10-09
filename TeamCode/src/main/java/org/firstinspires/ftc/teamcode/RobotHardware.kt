7fe3d5ffb01615b3a8562748e2c63b6b26b449d5
vlad <vlad.bintintan@gmail.com>
2024-10-09 16:48:36 +0300

    Add RobotHardware.kt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.kt
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.teamcode.hardware.MovementHardware
+
+class RobotHardware(hardwareMap: HardwareMap) : MovementHardware(hardwareMap)
