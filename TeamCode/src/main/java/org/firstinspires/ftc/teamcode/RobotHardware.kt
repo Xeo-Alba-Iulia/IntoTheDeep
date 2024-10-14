26ddb84cc87f6522288bd6181558499d5412c0a9
vlad <vlad.bintintan@gmail.com>
2024-10-14 22:52:57 +0300

    Change RobotHardware.kt to allow testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.kt
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.hardware.DcMotor
-import com.qualcomm.robotcore.hardware.Gamepad
 import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.teamcode.hardware.MotorHardwareInterface
 import org.firstinspires.ftc.teamcode.hardware.MovementHardware
 import org.firstinspires.ftc.teamcode.hardware.MovementHardwareInterface
 import org.firstinspires.ftc.teamcode.hardware.PendulHardware
@@ -14,39 +14,43 @@ import org.firstinspires.ftc.teamcode.hardware.PendulHardwareInterface
  *
  * @property movementHardware The movement hardware of the robot (All motors used in movement)
  * @param hardwareMap The hardware map from the OpMode
+ * @constructor Default constructs everything using just hardwareMap,
+ * but allows for custom hardware classes to be passed in for testing
  */
 class RobotHardware(
-    hardwareMap: HardwareMap
-) {
-    private val movementHardware: MovementHardwareInterface = MovementHardware(hardwareMap)
-    private val pendulHardware: PendulHardwareInterface = PendulHardware(hardwareMap)
-
-    // Function to move the robot using the gamepad
-    fun move(gamepad: Gamepad) {
-        movementHardware.move(gamepad)
-    }
-
-    // Set the power for both movement and pendul hardware
-    fun setPower(power: Double) {
-        movementHardware.setPower(power)
-        pendulHardware.setPower(power)
-    }
+    hardwareMap: HardwareMap,
+    private val movementHardware: MovementHardwareInterface = MovementHardware(hardwareMap),
+    private val pendulHardware: PendulHardwareInterface = PendulHardware(hardwareMap),
+):  MotorHardwareInterface,
+    MovementHardwareInterface by movementHardware,
+    PendulHardwareInterface by pendulHardware
+{
+    /**
+     * Since Java does not support default parameters, this constructor is provided for Java OpModes
+     *
+     * @param hardwareMap The hardware map from the OpMode
+     */
+    constructor(hardwareMap: HardwareMap): this(
+        hardwareMap,
+        MovementHardware(hardwareMap),
+        PendulHardware(hardwareMap),
+    )
 
     // Set the mode for both movement and pendul hardware
-    fun setMode(mode: DcMotor.RunMode) {
+    override fun setMode(mode: DcMotor.RunMode) {
         movementHardware.setMode(mode)
         pendulHardware.setMode(mode)
     }
 
     // Set the zero power behavior for both movement and pendul hardware
-    fun setZeroPowerBehavior(behavior: DcMotor.ZeroPowerBehavior) {
+    override fun setZeroPowerBehavior(behavior: DcMotor.ZeroPowerBehavior) {
         movementHardware.setZeroPowerBehavior(behavior)
         pendulHardware.setZeroPowerBehavior(behavior)
     }
 
-    fun pendul(gamepad: Gamepad) {
-        pendulHardware.pendul(gamepad)
+    // Set the power for both movement and pendul hardware
+    override fun setPower(power: Double) {
+        movementHardware.setPower(power)
+        pendulHardware.setPower(power)
     }
-
-    // Add any additional methods to control specific robot hardware components here
 }
