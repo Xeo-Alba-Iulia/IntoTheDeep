e5edb0013473f09de62c7122544d10e8805bd548
vlad <vlad.bintintan@gmail.com>
2024-11-19 16:30:06 +0200

    Change intake API
    Sexy plesa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Intake.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Intake.kt
@@ -3,25 +3,71 @@ package org.firstinspires.ftc.teamcode.hardware
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket
 import com.acmerobotics.roadrunner.Action
 import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.util.Range.clip
 
 class Intake(hardwareMap: HardwareMap) {
     private val intakeMotor = hardwareMap.crservo.get("IntakeMotor")
+//    private val servo = hardwareMap.servo.get("IntakeServo")
 
-    val runUntilCaught = object : Action {
-        private var isCanceled = false
+    private var intakePower = 0.2
+    private val startPower = 0.2
 
-        fun cancel() {
-            isCanceled = true
-        }
+    private var isCanceled = false
+    private var isStarted = false
 
+    inner class IntakeAction : Action {
         override fun run(p: TelemetryPacket): Boolean {
+            isStarted = true
             if (isCanceled) {
+                isStarted = false
+                isCanceled = false
+
                 intakeMotor.power = 0.0
                 return false
             }
-            intakeMotor.power = 0.1
 
+            intakeMotor.power = intakePower
             return true
         }
+    }
+
+    private fun actionWrapper(block: () -> Unit): Action = object : Action {
+        override fun run(p: TelemetryPacket): Boolean {
+            block()
+            return false
+        }
+    }
+
+    private fun actionWrapperTelemetry(block: (TelemetryPacket) -> Unit): Action = object : Action {
+        override fun run(p: TelemetryPacket): Boolean {
+            block(p)
+            return false
+        }
+    }
+
+    /**
+     * Starts the intake and resets the power
+     *
+     * @return [IntakeAction] object
+     * @throws IllegalStateException if the intake is already started
+     */
+    fun start() = if (!isStarted) {
+        intakePower = startPower
+        IntakeAction()
+    } else throw IllegalStateException("Intake already started")
+
+    /**
+     * Stops the intake
+     */
+    fun stop() = actionWrapper {
+        isCanceled = true
+    }
+
+    /**
+     * Sets power of intakeMotor, use only temporarily
+     */
+    fun setPower(power: Double) = actionWrapperTelemetry { packet ->
+        intakePower = clip(power, -1.0, 1.0)
+        packet.put("Intake Power changed", intakePower)
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/IntakeTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/IntakeTest.kt
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop
 
+import com.acmerobotics.roadrunner.ParallelAction
+import com.acmerobotics.roadrunner.SequentialAction
+import com.acmerobotics.roadrunner.SleepAction
 import com.acmerobotics.roadrunner.ftc.runBlocking
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
@@ -12,6 +15,11 @@ class IntakeTest : LinearOpMode() {
 
         waitForStart()
 
-        runBlocking(intake.runUntilCaught)
+        runBlocking(ParallelAction(intake.start(), SequentialAction(
+            SleepAction(1.0),
+            intake.setPower(0.5),
+            SleepAction(1.0),
+            intake.stop()
+        )))
     }
 }
