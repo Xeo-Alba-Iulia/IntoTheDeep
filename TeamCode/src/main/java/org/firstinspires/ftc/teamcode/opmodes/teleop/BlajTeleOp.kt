ff90cb826e28ab06755e4e584055c61a99ef31fc
vlad <vlad.bintintan@gmail.com>
2024-12-10 14:45:56 +0200

    TeleOp start

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BlajTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BlajTeleOp.kt
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop
 
+import com.acmerobotics.dashboard.FtcDashboard
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket
 import com.acmerobotics.roadrunner.Action
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
@@ -13,23 +14,37 @@ class BlajTeleOp : LinearOpMode() {
     lateinit var actionList: List<Action>
 
     internal fun runActions(telemetryPacket: TelemetryPacket) {
-
+        actionList.forEach { it.run(telemetryPacket) }
     }
 
     override fun runOpMode() {
         robotHardware = RobotHardware(hardwareMap)
 
         actionList = listOf(
-            robotHardware.intake
+            robotHardware.intake,
         )
 
         val moveGamepad = gamepad1
         val controlGamepad = gamepad2
 
+        val dashboard = FtcDashboard.getInstance()
+
         waitForStart()
 
         while(opModeIsActive()) {
+            // Movement
             robotHardware.move(moveGamepad)
+
+            // Actions for other hardware (intake, lift, etc)
+            val telemetryPacket = TelemetryPacket()
+            runActions(telemetryPacket)
+            dashboard.sendTelemetryPacket(telemetryPacket)
+
+            // Finite State Machine
+            when {
+                controlGamepad.a -> robotHardware.intake.start()
+                controlGamepad.b -> robotHardware.intake.cancel()
+            }
         }
     }
 }
