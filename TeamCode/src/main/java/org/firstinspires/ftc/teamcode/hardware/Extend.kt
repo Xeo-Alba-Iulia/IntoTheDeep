42582794d7f99daac2167a28e604d2c12a35cb9d
vlad <vlad.bintintan@gmail.com>
2024-12-12 19:45:28 +0200

    Refactor extend

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Extend.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Extend.kt
@@ -1,9 +1,8 @@
 package org.firstinspires.ftc.teamcode.hardware
 
 import com.qualcomm.robotcore.hardware.HardwareMap
-import com.qualcomm.robotcore.hardware.CRServo
 
-open class Extend(hardwareMap: HardwareMap) {
+class Extend(hardwareMap: HardwareMap) {
     private val extendLeft = hardwareMap.crservo.get("ExtendLeft")
     private val extendRight = hardwareMap.crservo.get("ExtendRight")
 
@@ -11,15 +10,11 @@ open class Extend(hardwareMap: HardwareMap) {
         extendLeft.direction.inverted()
     }
 
-    protected val servos = arrayListOf(extendLeft, extendRight)
+    val servos = arrayListOf(extendLeft, extendRight)
 
-    fun setPower(power: Double){
-        servos.forEach { it.power = power }
-    }
-}
-
-class ExtendTesting(hardwareMap: HardwareMap) : Extend(hardwareMap) {
-    fun test(position: Double) {
-        servos.forEach { it.power = position }
-    }
+    var power: Double
+        get() = extendLeft.power
+        set(value) {
+            servos.forEach { it.power = value }
+        }
 }
