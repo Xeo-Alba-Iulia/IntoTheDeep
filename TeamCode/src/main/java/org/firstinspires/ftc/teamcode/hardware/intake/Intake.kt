8f4426c910005f04c790aabfabed3b96c19bdb57
vlad <vlad.bintintan@gmail.com>
2024-12-13 12:49:16 +0200

    Refactor intake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/intake/Intake.kt
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.hardware.intake
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket
+import com.acmerobotics.roadrunner.Action
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.teamcode.hardware.ManualPositionMechanism
+
+class Intake(hardwareMap: HardwareMap) : Action {
+    private val intakeMotor = hardwareMap.crservo.get("IntakeMotor")
+
+    init {
+        intakeMotor.direction = DcMotorSimple.Direction.REVERSE
+    }
+
+    // Implementing Action interface for the actual Intake
+    private var isCanceled = false
+
+    /**
+     * Power to set the intake to
+     */
+    var intakePower: Double = 0.6
+        set(value) {
+            require(value in -1.0..1.0) { "Intake power must be between 0 and 1" }
+            field = value
+        }
+
+    override fun run(p: TelemetryPacket): Boolean {
+        intakeMotor.power = intakePower
+        return true
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/intake/IntakeRotationPosition.kt
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.hardware.intake
+
+enum class IntakeRotationPosition(val positionValue: Double) {
+    PARALLEL(0.4),
+    PERPENDICULAR(0.65),
+}
