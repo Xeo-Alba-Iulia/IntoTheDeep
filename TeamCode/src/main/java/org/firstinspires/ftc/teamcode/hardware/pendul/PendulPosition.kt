a5ea06559dc6854ef2537ea885cb88f2141ea1df
vlad <vlad.bintintan@gmail.com>
2024-12-13 17:45:11 +0200

    DOWN pendul position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/pendul/PendulPosition.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/pendul/PendulPosition.kt
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.hardware.pendul
 
+/**
+ * **Poziții pentru coșul de sus și bara a 2-a**
+ */
 enum class PendulPosition(val positionValue: Double) {
-    DOWN(0.2),
-    HALF(0.24),
-    UP(0.66)
+    DOWN(0.29),
+    BASKET(0.8),
+    BAR(0.89)
 }
