f592d3ba9ca095e90e5e9cee1000ddda22f16338
vlad <vlad.bintintan@gmail.com>
2024-12-13 12:32:21 +0200

    Add ExtendTest.kt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/tests/ExtendTest.kt
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop.tests
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.hardware.Extend
+
+const val MULTIPLIER = 0.001
+
+@TeleOp
+class ExtendTest : LinearOpMode() {
+    override fun runOpMode() {
+        val extend = Extend(hardwareMap)
+
+        waitForStart()
+
+        while (opModeIsActive()) {
+            extend.power = gamepad1.right_stick_y * MULTIPLIER
+            extend.run(TelemetryPacket())
+        }
+    }
+
+}
