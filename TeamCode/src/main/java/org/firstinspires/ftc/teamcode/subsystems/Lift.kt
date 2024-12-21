eff573cc694c723aa5dd4b28210d16187358ae4f
vlad <vlad.bintintan@gmail.com>
2024-12-22 00:45:42 +0200

    Motion profile lift for one height

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.kt
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.subsystems
 
 import com.acmerobotics.dashboard.config.Config
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket
+import com.acmerobotics.roadrunner.Action
+import com.acmerobotics.roadrunner.constantProfile
 import com.acmerobotics.roadrunner.ftc.RawEncoder
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorEx
@@ -17,7 +19,7 @@ import org.firstinspires.ftc.teamcode.control.PIDFController
  * @param isVerbose whether to print debug information
  */
 @Config
-class Lift(hardwareMap: HardwareMap, private val isVerbose: Boolean = true) : ManualPositionMechanism {
+class Lift(hardwareMap: HardwareMap, private val isVerbose: Boolean = true) : Action {
     companion object {
         @JvmField
         @Volatile
@@ -32,24 +34,45 @@ class Lift(hardwareMap: HardwareMap, private val isVerbose: Boolean = true) : Ma
         @JvmField
         @Volatile
         var kA = 0.0
+
+        @JvmField
+        @Volatile
+        var height = 100.0
     }
+
+    private enum class SimpleState {
+        DOWN, UP
+    }
+
+    private enum class State {
+        DOWN {
+            override val simpleState = SimpleState.DOWN
+        },
+        GOING_DOWN {
+            override val simpleState = SimpleState.DOWN
+        },
+        UP {
+            override val simpleState = SimpleState.UP
+        },
+        GOING_UP {
+            override val simpleState = SimpleState.UP
+        };
+
+        abstract val simpleState: SimpleState
+    }
+
+    private var state = State.DOWN
 
     private val liftLeft: DcMotorEx = hardwareMap.get(DcMotorEx::class.java, "LiftLeft")
     private val liftRight: DcMotorEx = hardwareMap.get(DcMotorEx::class.java, "LiftRight")
 
     val encoder = RawEncoder(liftRight)
 
-    private var isCanceled = false
-
     private val lift = listOf(liftLeft, liftRight)
 
     private val controller = PIDFController(coefficients, kV, kA, kStatic)
 
-    override var targetPosition
-        get() = controller.targetPosition
-        set(value) {
-            controller.targetPosition = value
-        }
+    private val profile = constantProfile(height, 0.0, 400.0, -100.0, 100.0).baseProfile
 
     /**
      * Actual position of the lift, as measured while the action is running
@@ -66,51 +89,79 @@ class Lift(hardwareMap: HardwareMap, private val isVerbose: Boolean = true) : Ma
     init {
         lift.forEach {
             it.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
-            it.mode = DcMotor.RunMode.RUN_USING_ENCODER
-            it.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
-            it.mode = DcMotor.RunMode.RUN_USING_ENCODER
         }
 
         liftRight.direction = DcMotorSimple.Direction.REVERSE
         encoder.direction = DcMotorSimple.Direction.REVERSE
-
-        controller.targetVelocity = 10.0
     }
 
     var power = 0.0
 
-    override fun cancel() {
-        isCanceled = true
-    }
-
     override fun run(p: TelemetryPacket): Boolean {
-        if (isCanceled) {
-            isCanceled = false
-            power = 0.0
-
-            return false
+        if (state == State.GOING_DOWN && measuredPosition < 10.0) {
+            state = State.DOWN
+        } else if (state == State.GOING_UP && measuredPosition > height - 10.0) {
+            state = State.UP
         }
 
-//        val positionVelocityPair = encoder.getPositionAndVelocity()
-//        measuredPosition = positionVelocityPair.position.toDouble()
-//        measuredVelocity = positionVelocityPair.velocity.toDouble()
-//
-//        power = controller.update(measuredPosition, measuredVelocity)
-//
-//        if (isVerbose) {
-//            p.putAll(
-//                mapOf(
-//                    "liftPosition" to measuredPosition,
-//                    "liftVelocity" to measuredVelocity,
-//                    "liftPower" to power
-//                )
-//            )
+        val positionVelocityPair = encoder.getPositionAndVelocity()
+        measuredPosition = positionVelocityPair.position.toDouble()
+        measuredVelocity = positionVelocityPair.velocity.toDouble()
+
+        val (targetPosition, targetVelocity, targetAcceleration) = when (state.simpleState) {
+            SimpleState.UP -> Triple(
+                profile[measuredPosition][0],
+                profile[measuredPosition][1],
+                profile[measuredPosition][2]
+            )
+
+            SimpleState.DOWN -> Triple(
+                height - profile[height - measuredPosition][0],
+                -(height - profile[height - measuredPosition][1]),
+                -(height - profile[height - measuredPosition][2])
+            )
+        }
+
+        controller.targetPosition = targetPosition
+        controller.targetVelocity = targetVelocity
+        controller.targetAcceleration = targetAcceleration
+
+        if (isVerbose) {
+            p.putAll(
+                mapOf(
+                    "liftPosition" to measuredPosition,
+                    "liftVelocity" to measuredVelocity,
+                    "liftPower" to power
+                )
+            )
+        }
+
+//        for (motor in lift) {
+//            motor.power = power
 //        }
 
-        for (motor in lift) {
-            motor.power = power
-        }
-
         return true
+    }
+
+    /**
+     * Makes the lift go up if it is currently down
+     *
+     * Otherwise, does nothing
+     */
+    fun goUp() {
+        if (state == State.DOWN) {
+            state = State.GOING_UP
+        }
+    }
+
+    /**
+     * Makes the lift go down if it is currently up
+     *
+     * Otherwise, does nothing
+     */
+    fun goDown() {
+        if (state == State.UP) {
+            state = State.GOING_DOWN
+        }
     }
 }
