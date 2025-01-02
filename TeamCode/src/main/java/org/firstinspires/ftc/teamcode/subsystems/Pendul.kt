523b785fd0e5f0453328f5b0439f623abc2b2d28
vlad <vlad.bintintan@gmail.com>
2025-01-02 16:33:23 +0200

    Schimbare pendul la un servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Pendul.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Pendul.kt
@@ -2,19 +2,11 @@ package org.firstinspires.ftc.teamcode.subsystems
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket
 import com.qualcomm.robotcore.hardware.HardwareMap
-import com.qualcomm.robotcore.hardware.Servo
 
 class Pendul(hardwareMap: HardwareMap) : ManualPositionMechanism {
-    private val servoLeft = hardwareMap.servo.get("PendulLeft")
-    private val servoRight = hardwareMap.servo.get("PendulRight")
-
     private var isCanceled = false
 
-    init {
-       servoLeft.direction = Servo.Direction.REVERSE
-    }
-
-    private val servos = arrayListOf(servoLeft, servoRight)
+    private val servos = arrayOf(hardwareMap.servo.get("Pendul"))
 
     override fun cancel() {
         isCanceled = true
