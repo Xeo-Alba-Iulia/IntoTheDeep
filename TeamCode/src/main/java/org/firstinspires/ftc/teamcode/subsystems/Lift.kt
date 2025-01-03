ef27fbaada8bb9be29790effd8e59e84a28b640c
vlad <vlad.bintintan@gmail.com>
2025-01-03 12:46:53 +0200

    Rearrange imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.kt
@@ -13,10 +13,10 @@ import org.firstinspires.ftc.teamcode.control.PIDFController
 import org.firstinspires.ftc.teamcode.profile.MotionProfileGenerator
 import org.firstinspires.ftc.teamcode.profile.MotionState
 import org.firstinspires.ftc.teamcode.util.absoluteDistance
-
-private const val PIDHeight = 100.0
 import org.firstinspires.ftc.teamcode.subsystems.util.ManualMechanismTeleOp
 import org.firstinspires.ftc.teamcode.subsystems.util.ManualPositionMechanism
+
+private const val PIDHeight = 100.0
 
 /**
  * Lift subsystem
