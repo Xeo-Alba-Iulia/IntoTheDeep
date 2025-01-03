1f6edf20f73c58ae00fa64523a0b8799366df08c
vlad <vlad.bintintan@gmail.com>
2025-01-03 17:31:50 +0200

    Max extend position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Extend.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Extend.kt
@@ -2,8 +2,11 @@ package org.firstinspires.ftc.teamcode.subsystems
 
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.Servo
-import org.firstinspires.ftc.teamcode.subsystems.util.ServoPositionMechanism
+import com.qualcomm.robotcore.util.Range
 import org.firstinspires.ftc.teamcode.subsystems.util.Positions
+import org.firstinspires.ftc.teamcode.subsystems.util.ServoPositionMechanism
+
+private const val MAX_POSITION = 0.9
 
 class Extend(hardwareMap: HardwareMap) : ServoPositionMechanism(Positions.Extend.`in`) {
     companion object {
@@ -11,6 +14,12 @@ class Extend(hardwareMap: HardwareMap) : ServoPositionMechanism(Positions.Extend
     }
 
     override val servos: Array<Servo> = arrayOf(hardwareMap.servo["Extend"])
+
+    override var targetPosition: Double
+        get() = super.targetPosition
+        set(value) {
+            super.targetPosition = Range.clip(value, 0.0, MAX_POSITION)
+        }
 }
 
 //@TeleOp(group = "Servo Subsystems")
