afd91d6160870323ae02bd5013e1d97f2dc6d1b1
vlad <vlad.bintintan@gmail.com>
2025-01-04 16:21:02 +0200

    Init position IntakePendul

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakePendul.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakePendul.kt
@@ -3,11 +3,10 @@ package org.firstinspires.ftc.teamcode.subsystems
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.Servo
-import org.firstinspires.ftc.teamcode.subsystems.util.ServoPositionMechanism
 import org.firstinspires.ftc.teamcode.subsystems.util.ManualMechanismTeleOp
-import org.firstinspires.ftc.teamcode.subsystems.util.Positions
+import org.firstinspires.ftc.teamcode.subsystems.util.ServoPositionMechanism
 
-class IntakePendul(hardwareMap: HardwareMap) : ServoPositionMechanism(0.69) {
+class IntakePendul(hardwareMap: HardwareMap) : ServoPositionMechanism(0.9) {
     override val servos: Array<Servo> = arrayOf(hardwareMap.servo["IntakePendul"])
 }
 
