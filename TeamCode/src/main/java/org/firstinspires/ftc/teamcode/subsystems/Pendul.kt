97518af94f514cc27c7ff52fda6283a7ddf15740
vlad <vlad.bintintan@gmail.com>
2025-01-04 13:50:57 +0200

    Modify pendul init position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Pendul.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Pendul.kt
@@ -7,7 +7,7 @@ import org.firstinspires.ftc.teamcode.subsystems.util.ServoPositionMechanism
 import org.firstinspires.ftc.teamcode.subsystems.util.ManualMechanismTeleOp
 import org.firstinspires.ftc.teamcode.subsystems.util.Positions
 
-class Pendul(hardwareMap: HardwareMap) : ServoPositionMechanism(0.6) {
+class Pendul(hardwareMap: HardwareMap) : ServoPositionMechanism(0.66) {
     override val servos: Array<Servo> = arrayOf(hardwareMap.servo["Pendul"])
 
     companion object {
