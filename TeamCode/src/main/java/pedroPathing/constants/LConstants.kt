5ea9c19df1891ffdf676b9298417f051e482f2b4
vlad <vlad.bintintan@gmail.com>
2025-01-14 17:07:05 +0200

    PedroPathing library

---
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.kt
@@ -0,0 +1,25 @@
+package pedroPathing.constants
+
+import com.pedropathing.follower.FollowerConstants
+import com.pedropathing.localization.Encoder
+import com.pedropathing.localization.Localizers
+import com.pedropathing.localization.constants.ThreeWheelConstants
+
+@Suppress("unused")
+object LConstants {
+    init {
+        FollowerConstants.localizers = Localizers.THREE_WHEEL
+        ThreeWheelConstants.forwardTicksToInches = 0.00052189
+        ThreeWheelConstants.strafeTicksToInches = 0.00052189
+        ThreeWheelConstants.turnTicksToInches = 0.00053717
+        ThreeWheelConstants.leftY = 3.70079
+        ThreeWheelConstants.rightY = -3.70079
+        ThreeWheelConstants.strafeX = -5.67
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "MotorBackLeft"
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "LiftLeft"
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "MotorBackRight"
+        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE
+        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE
+        ThreeWheelConstants.strafeEncoderDirection = Encoder.REVERSE
+    }
+}
