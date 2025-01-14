169f02b34e36c8ae061a67acbffb82ba7e5cea78
vlad <vlad.bintintan@gmail.com>
2025-01-14 15:30:07 +0200

    Add comment in CancelableAction.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/util/CancelableAction.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/util/CancelableAction.kt
@@ -3,5 +3,8 @@ package org.firstinspires.ftc.teamcode.subsystems.util
 import com.acmerobotics.roadrunner.Action
 
 interface CancelableAction : Action {
+    /**
+     * Stops the action as soon as possible
+     */
     fun cancel()
 }
