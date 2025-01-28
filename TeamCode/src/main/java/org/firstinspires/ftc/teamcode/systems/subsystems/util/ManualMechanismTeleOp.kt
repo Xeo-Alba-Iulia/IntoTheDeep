1025b044c8f264469f78a0b923e1320944314668
vlad <vlad.bintintan@gmail.com>
2025-01-28 15:50:08 +0200

    Custom multiplier in ManuamMechanismTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/subsystems/util/ManualMechanismTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/subsystems/util/ManualMechanismTeleOp.kt
@@ -4,20 +4,20 @@ import com.acmerobotics.dashboard.FtcDashboard
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 
-private const val MULTIPLIER = 0.003
-
 abstract class ManualMechanismTeleOp(
     private val factory: ManualPositionFactory,
 ) : OpMode() {
     protected lateinit var manualPositionMechanism: ManualPositionMechanism
     val dashboard = FtcDashboard.getInstance() as FtcDashboard
 
+    protected open val multiplier = 0.003
+
     override fun init() {
         manualPositionMechanism = factory.manualPositionFactory(hardwareMap)
     }
 
     override fun loop() {
-        manualPositionMechanism.targetPosition -= MULTIPLIER * gamepad1.left_stick_y
+        manualPositionMechanism.targetPosition -= multiplier * gamepad1.left_stick_y
 
         val packet = TelemetryPacket()
 
