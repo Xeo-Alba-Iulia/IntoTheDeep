50041a96a91a4e99b3dac5eab37a1f9778f4959d
vlad <vlad.bintintan@gmail.com>
2025-01-29 10:28:38 +0200

    Update encoder constants and motor names

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -7,15 +7,15 @@ public class LConstants {
     static {
         ThreeWheelConstants.forwardTicksToInches = 0.002959498594166741;
         ThreeWheelConstants.strafeTicksToInches = -0.00314368383040309;
-        ThreeWheelConstants.turnTicksToInches = 0.0029836080519608822;
+        ThreeWheelConstants.turnTicksToInches = 0.00291696596614687;
         ThreeWheelConstants.leftY = 5.314961;
         ThreeWheelConstants.rightY = -5.314961;
-        ThreeWheelConstants.strafeX = -3.937017;
-        ThreeWheelConstants.leftEncoder_HardwareMapName = "LiftLeft";
-        ThreeWheelConstants.rightEncoder_HardwareMapName = "MotorBackLeft";
-        ThreeWheelConstants.strafeEncoder_HardwareMapName = "MotorBackRight";
-        ThreeWheelConstants.leftEncoderDirection = Encoder.FORWARD;
-        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.strafeX = 4.01574803;
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "MotorFrontLeft";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "MotorFrontRight";
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "MotorBackLeft";
+        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.rightEncoderDirection = Encoder.FORWARD;
         ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
     }
 }
