a2489c81f7ba57a840e9a64a6d48f96e021e7ca9
vlad <vlad.bintintan@gmail.com>
2025-01-29 15:54:34 +0200

    Add drive to ManualMechanismTeleOp.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/subsystems/util/ManualMechanismTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/subsystems/util/ManualMechanismTeleOp.kt
@@ -2,21 +2,45 @@ package org.firstinspires.ftc.teamcode.systems.subsystems.util
 
 import com.acmerobotics.dashboard.FtcDashboard
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket
+import com.pedropathing.follower.Follower
+import com.pedropathing.localization.Pose
+import com.pedropathing.util.Constants
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import pedroPathing.constants.FConstants
+import pedroPathing.constants.LConstants
 
 abstract class ManualMechanismTeleOp(
     private val factory: ManualPositionFactory,
 ) : OpMode() {
     protected lateinit var manualPositionMechanism: ManualPositionMechanism
     val dashboard = FtcDashboard.getInstance() as FtcDashboard
+    lateinit var follower: Follower
+
     protected open val multiplier = 0.003
 
     override fun init() {
+        Constants.setConstants(FConstants::class.java, LConstants::class.java)
+        follower = Follower(hardwareMap)
+        follower.setStartingPose(Pose(0.0, 0.0, 0.0))
+
         manualPositionMechanism = factory.manualPositionFactory(hardwareMap)
     }
 
+    override fun start() {
+        follower.startTeleopDrive()
+    }
+
     override fun loop() {
-        manualPositionMechanism.targetPosition -= multiplier * gamepad1.left_stick_y
+        follower.setTeleOpMovementVectors(
+            -gamepad1.left_stick_y.toDouble(),
+            -gamepad1.left_stick_x.toDouble(),
+            -gamepad1.right_stick_x.toDouble(),
+            true
+        )
+        follower.update()
+
+        manualPositionMechanism.targetPosition +=
+            multiplier * (gamepad1.right_trigger - gamepad1.left_trigger)
 
         val packet = TelemetryPacket()
 
