74ba55f50fad766afba864e481a33a2a47fc20e8
vlad <vlad.bintintan@gmail.com>
2025-02-03 09:49:06 +0200

    Pozitii tunabile din dashboard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/subsystems/util/Positions.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/subsystems/util/Positions.kt
@@ -2,95 +2,119 @@ package org.firstinspires.ftc.teamcode.systems.subsystems.util
 
 import com.acmerobotics.dashboard.config.Config
 
-@Config
 object Positions {
-    object IntakeRotation {
-        @JvmField
-        @Volatile
-        var parallel = 0.345
+    @Config
+    class IntakeRotation {
+        companion object {
+            @JvmField
+            @Volatile
+            var parallel = 0.345
 
-        @JvmField
-        @Volatile
-        var perpendicular = 0.6466
+            @JvmField
+            @Volatile
+            var perpendicular = 0.6466
+        }
     }
 
-    object Pendul {
-        @JvmField
-        @Volatile
-        var transfer = 0.0
+    @Config
+    class Pendul {
+        companion object {
+            @JvmField
+            @Volatile
+            var transfer = 0.0
 
-        @JvmField
-        @Volatile
-        var bar = 0.71
+            @JvmField
+            @Volatile
+            var bar = 0.71
 
-        @JvmField
-        @Volatile
-        var basket = 0.31
+            @JvmField
+            @Volatile
+            var basket = 0.31
 
-        var pickup = 1.0
+            @JvmField
+            @Volatile
+            var pickup = 1.0
+        }
     }
 
-    object Lift {
-        @JvmField
-        @Volatile
-        var down = 0.0
+    @Config
+    class Lift {
+        companion object {
+            @JvmField
+            @Volatile
+            var down = 0.0
 
-        @JvmField
-        @Volatile
-        var half = 450.0
+            @JvmField
+            @Volatile
+            var half = 450.0
 
-        @JvmField
-        @Volatile
-        var up = 2150.0
+            @JvmField
+            @Volatile
+            var up = 2150.0
+        }
     }
 
-    object Extend {
-        @JvmField
-        @Volatile
-        var `in` = 0.32
+    @Config
+    class Extend {
+        companion object {
+            @JvmField
+            @Volatile
+            var `in` = 0.32
 
-        @JvmField
-        @Volatile
-        var out = 0.6
+            @JvmField
+            @Volatile
+            var out = 0.6
+        }
     }
 
-    object IntakePendul {
-        @JvmField
-        @Volatile
-        var down = 0.43
+    @Config
+    class IntakePendul {
+        companion object {
+            @JvmField
+            @Volatile
+            var down = 0.43
 
-        @JvmField
-        @Volatile
-        var entrance = 0.50
+            @JvmField
+            @Volatile
+            var entrance = 0.50
 
-        @JvmField
-        @Volatile
-        var up = 0.84
+            @JvmField
+            @Volatile
+            var up = 0.84
+        }
     }
 
-    object Claw {
-        @JvmField
-        @Volatile
-        var open: Double = 0.469
+    @Config
+    class Claw {
+        companion object {
+            @JvmField
+            @Volatile
+            var open: Double = 0.469
 
-        @JvmField
-        @Volatile
-        var close: Double = 0.621
+            @JvmField
+            @Volatile
+            var close: Double = 0.621
+        }
     }
 
-    object ClawRotate {
-        @JvmField
-        @Volatile
-        var transfer: Double = 0.9
+    @Config
+    class ClawRotate {
+        companion object {
+            @JvmField
+            @Volatile
+            var transfer: Double = 0.9
 
-        @JvmField
-        @Volatile
-        var bar: Double = 0.2
+            @JvmField
+            @Volatile
+            var bar: Double = 0.2
 
-        @JvmField
-        @Volatile
-        var basket: Double = 0.7665
+            @JvmField
+            @Volatile
+            var basket: Double = 0.7665
 
-        var pickup = 0.23
+            @JvmField
+            @Volatile
+            var pickup = 0.23
+        }
     }
 }
