cefd3fd002c6cd8fceb4d11da2b6d70ac0172c3e
yes <rat887444@gmail.com>
2025-02-04 18:02:09 +0200

    Constante fara turn si teste schimbate

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -5,12 +5,12 @@ import com.pedropathing.localization.constants.ThreeWheelConstants;
 
 public class LConstants {
     static {
-        ThreeWheelConstants.forwardTicksToInches = 0.002959498594166741;
-        ThreeWheelConstants.strafeTicksToInches = 0.00314368383040309;
+        ThreeWheelConstants.forwardTicksToInches = 0.0029781667433598;
+        ThreeWheelConstants.strafeTicksToInches = -0.003049147237455;
         ThreeWheelConstants.turnTicksToInches = 0.00291696596614687;
         ThreeWheelConstants.leftY = 5.314961;
         ThreeWheelConstants.rightY = -5.314961;
-        ThreeWheelConstants.strafeX = 4.01574803;
+        ThreeWheelConstants.strafeX = 3.74016;
         ThreeWheelConstants.leftEncoder_HardwareMapName = "MotorBackLeft";
         ThreeWheelConstants.rightEncoder_HardwareMapName = "MotorFrontRight";
         ThreeWheelConstants.strafeEncoder_HardwareMapName = "MotorFrontLeft";
