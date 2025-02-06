1a352fa1f8e511a8e757c44f9d33b18193aef25e
vlad <vlad.bintintan@gmail.com>
2025-02-06 07:58:24 +0200

    Add Claw#isClosed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/subsystems/outtake/Claw.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/subsystems/outtake/Claw.kt
@@ -4,18 +4,24 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.teamcode.systems.subsystems.util.ManualMechanismTeleOp
-import org.firstinspires.ftc.teamcode.systems.subsystems.util.Positions
+import org.firstinspires.ftc.teamcode.systems.subsystems.util.Positions.Claw.Companion.close
+import org.firstinspires.ftc.teamcode.systems.subsystems.util.Positions.Claw.Companion.open
 import org.firstinspires.ftc.teamcode.systems.subsystems.util.ServoPositionMechanism
 
 class Claw(
     hardwareMap: HardwareMap,
-) : ServoPositionMechanism(Positions.Claw.close) {
+) : ServoPositionMechanism(close) {
     override val servos: Array<Servo> = arrayOf(hardwareMap.servo["Claw"])
 
     init {
-        targetPosition = Positions.Claw.open
         servos[0].direction = Servo.Direction.REVERSE
     }
+
+    var isClosed
+        get() = targetPosition == close
+        set(value) {
+            targetPosition = if (value) close else open
+        }
 }
 
 @TeleOp(name = "Claw test", group = "C")
