27f4dd281fba1f334a71dd935f8cf5eec9184f87
vlad <vlad.bintintan@gmail.com>
2025-02-06 07:59:24 +0200

    Face RobotHardware o actiune ce ruleaza toate actiunile robotului

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.kt
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode
 
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket
+import com.acmerobotics.roadrunner.Action
 import com.pedropathing.util.Constants
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.teamcode.systems.Outtake
@@ -29,8 +31,16 @@ class RobotHardware
         val intake: IntakeMotor = IntakeMotor(hardwareMap),
         val extend: Extend = Extend(hardwareMap),
         val claw: Claw = Claw(hardwareMap),
-    ) {
+    ) : Action {
         init {
             Constants.setConstants(FConstants::class.java, LConstants::class.java)
         }
+
+        override fun run(p: TelemetryPacket) =
+            lift.run(p) &&
+                outtake.run(p) &&
+                intakePendul.run(p) &&
+                intake.run(p) &&
+                claw.run(p) &&
+                extend.run(p)
     }
