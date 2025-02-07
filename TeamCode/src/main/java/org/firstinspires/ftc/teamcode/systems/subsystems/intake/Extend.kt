aada163994b593f21b2ce6212eb03ccfbd1933c1
vlad <vlad.bintintan@gmail.com>
2025-02-07 22:07:18 +0200

    Reverse extend positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/subsystems/intake/Extend.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/subsystems/intake/Extend.kt
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.teamcode.systems.subsystems.intake
 
 import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.teamcode.systems.subsystems.util.ServoPositionMechanism
 
 class Extend(
     hardwareMap: HardwareMap,
 ) : ServoPositionMechanism(0.0) {
     override val servos = arrayOf(hardwareMap.servo["Extend1"], hardwareMap.servo["Extend2"])
+
+    init {
+        servos.forEach { it.direction = Servo.Direction.REVERSE }
+    }
 }
