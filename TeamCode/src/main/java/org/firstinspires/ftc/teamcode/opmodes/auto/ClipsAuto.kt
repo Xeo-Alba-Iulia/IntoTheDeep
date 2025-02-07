6be8c05c5bd4981575703194428666751483b0c8
vlad <vlad.bintintan@gmail.com>
2025-02-07 11:12:01 +0200

    Auto 2 specimene

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/ClipsAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/ClipsAuto.kt
@@ -33,20 +33,57 @@ class ClipsAuto : LinearOpMode() {
 
         val dashboard = FtcDashboard.getInstance()
 
-        val beginPose = Pose(8.2, 64.5, Math.toRadians(180.0))
-        val scorePose = Pose(38.5, 70.0, Math.toRadians(180.0))
+        val beginPose = Pose(8.2, 60.0, Math.toRadians(180.0))
+        val scorePose = Pose(38.5, 72.5, Math.toRadians(180.001))
         val pickup1Intermediary = Pose(36.0, 36.0, 0.0)
         val pickup1Pose = Pose(60.3, 29.06, 0.0)
-        val drop1Pose = Pose(18.0, 28.0, 0.0)
+        val drop1Pose = Pose(30.0, 28.0, 0.0)
         val pickup2Pose = Pose(60.3, 20.5, 0.0)
         val pickup2Intermediary = Point(62.3, 30.0)
-        val drop2Pose = Pose(18.0, 20.5, 0.0)
+        val drop2Pose = Pose(30.0, 20.5, 0.0)
         val pickup3Pose = Pose(60.3, 13.0, 0.0)
         val pickup3Intermediary = Point(62.3, 20.0)
         val drop3Pose = Pose(18.0, 13.0, 0.0)
 
+        val dropControl = Point(30.0, 24.0)
+        val humanPickup = Pose(12.7, 24.0, 0.0)
+
         val scorePreloadPath = Path(BezierLine(Point(beginPose), Point(scorePose)))
         scorePreloadPath.setConstantHeadingInterpolation(Math.toRadians(180.0))
+        scorePreloadPath.zeroPowerAccelerationMultiplier = 8.0
+
+        scorePose.y += 2.5
+
+        val scorePaths =
+            Array(4) {
+                val newScorePose = scorePose.copy()
+                newScorePose.y -= (0.5) * it
+
+                val scoreControl = Point(humanPickup.x, newScorePose.y)
+                val scoreControl2 = Point(25.0, newScorePose.y)
+
+                val goPath =
+                    follower
+                        .pathBuilder()
+                        .addPath(
+                            BezierCurve(Point(humanPickup), scoreControl, scoreControl2)
+                        ).setLinearHeadingInterpolation(humanPickup.heading, newScorePose.heading)
+                        .addPath(
+                            BezierLine(scoreControl2, Point(newScorePose))
+                        ).setConstantHeadingInterpolation(newScorePose.heading)
+                        .build()
+
+                val returnPath =
+                    follower
+                        .pathBuilder()
+                        .addPath(BezierLine(Point(newScorePose), scoreControl2))
+                        .setConstantHeadingInterpolation(newScorePose.heading)
+                        .addPath(BezierCurve(scoreControl2, dropControl, Point(humanPickup)))
+                        .setLinearHeadingInterpolation(newScorePose.heading, humanPickup.heading)
+                        .build()
+
+                Pair(goPath, returnPath)
+            }
 
         val drop1Path =
             follower
@@ -58,25 +95,25 @@ class ClipsAuto : LinearOpMode() {
                 .setConstantHeadingInterpolation(0.0)
                 .addPath(BezierLine(Point(pickup1Pose), Point(drop1Pose)))
                 .setConstantHeadingInterpolation(0.0)
-                .build()
 
         val drop2Path =
-            follower
-                .pathBuilder()
+            drop1Path
                 .addPath(BezierCurve(Point(drop1Pose), pickup2Intermediary, Point(pickup2Pose)))
                 .setConstantHeadingInterpolation(0.0)
                 .addPath(BezierLine(Point(pickup2Pose), Point(drop2Pose)))
                 .setConstantHeadingInterpolation(0.0)
-                .build()
 
         val drop3Path =
-            follower
-                .pathBuilder()
+            drop2Path
                 .addPath(BezierCurve(Point(drop2Pose), pickup3Intermediary, Point(pickup3Pose)))
                 .setConstantHeadingInterpolation(0.0)
                 .addPath(BezierLine(Point(pickup3Pose), Point(drop3Pose)))
                 .setConstantHeadingInterpolation(0.0)
-                .build()
+
+        val dropPaths = drop3Path.build()
+
+        val humanPickupPath = Path(BezierCurve(Point(drop3Pose), dropControl, Point(humanPickup)))
+        humanPickupPath.setConstantHeadingInterpolation(0.0)
 
         follower.setStartingPose(beginPose)
 
@@ -105,26 +142,42 @@ class ClipsAuto : LinearOpMode() {
                         robot.claw.isClosed = false
                         robot.lift.targetPosition = Positions.Lift.down
 
-                        follower.followPath(drop1Path)
-                        state = 3
+                        follower.followPath(dropPaths, false)
+                        state = 1
                     }
                 }
 
-                3 -> {
+                1 -> {
                     if (pathTimer.elapsedTimeSeconds >= 1.0) {
                         robot.outtake.outtakePosition = OuttakePosition.PICKUP
                     }
 
                     if (!follower.isBusy) {
-                        follower.followPath(drop2Path)
+                        follower.followPath(humanPickupPath)
+                        state = 3
+                    }
+                }
+
+                3 -> {
+                    if (!follower.isBusy) {
+                        robot.claw.isClosed = true
+                        robot.outtake.outtakePosition = OuttakePosition.BAR
+                        robot.lift.targetPosition = Positions.Lift.half
+
                         state = 4
                     }
                 }
 
                 4 -> {
-                    if (!follower.isBusy) {
-                        follower.followPath(drop3Path)
+                    if (pathTimer.elapsedTimeSeconds > 0.5) {
+                        follower.followPath(scorePaths[0].first)
                         state = 5
+                    }
+                }
+
+                5 -> {
+                    if (!follower.isBusy) {
+                        requestOpModeStop()
                     }
                 }
             }
