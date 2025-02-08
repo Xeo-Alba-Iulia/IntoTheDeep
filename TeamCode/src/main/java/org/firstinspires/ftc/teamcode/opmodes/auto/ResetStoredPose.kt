1786738d3c13b586ba0c5e7b0dc60188eb0f9a39
vlad <vlad.bintintan@gmail.com>
2025-02-08 08:43:26 +0200

    Auto-TeleOp Position store

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/ResetStoredPose.kt
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto
+
+import com.pedropathing.localization.Pose
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.firstinspires.ftc.teamcode.util.PositionStore
+
+@Autonomous
+class ResetStoredPose : LinearOpMode() {
+    override fun runOpMode() {
+        waitForStart()
+
+        PositionStore.pose = Pose(0.0, 0.0, 0.0)
+        this.requestOpModeStop()
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PositionStore.kt
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.util
+
+import com.pedropathing.localization.Pose
+
+class PositionStore {
+    companion object {
+        var pose = Pose(0.0, 0.0, 0.0)
+    }
+}
