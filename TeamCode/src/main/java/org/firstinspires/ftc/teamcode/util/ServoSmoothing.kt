16121d11de9cc015387b58dfdacca22c144d50be
vlad <vlad.bintintan@gmail.com>
2025-02-10 20:30:24 +0200

    Remove useless object

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ServoSmoothing.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ServoSmoothing.kt
@@ -1,11 +1,9 @@
 package org.firstinspires.ftc.teamcode.util
 
-object ServoSmoothing {
-    fun servoSmoothing(
-        currPos: Double,
-        targetPos: Double,
-    ): Double {
-        val smoothedPos = (targetPos * 0.5) + (currPos * 0.5)
-        return smoothedPos
-    }
+fun servoSmoothing(
+    currPos: Double,
+    targetPos: Double,
+): Double {
+    val smoothedPos = (targetPos * 0.5) + (currPos * 0.5)
+    return smoothedPos
 }
