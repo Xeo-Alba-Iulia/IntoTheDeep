44c776f7e7fb456fff641da3b8a51a95f2762f04
vlad <vlad.bintintan@gmail.com>
2025-02-15 08:44:18 +0200

    PID Final TXS

--- TeamCode/src/main/java/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -26,19 +26,22 @@ public class FConstants {
         FollowerConstants.forwardZeroPowerAcceleration = -41.78295044292578;
         FollowerConstants.lateralZeroPowerAcceleration = -73.40248127111744;
 
-        FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.14,0,0.005,0);
+        FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.25,0,0.015,0);
         FollowerConstants.useSecondaryTranslationalPID = false;
         FollowerConstants.secondaryTranslationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
 
-        FollowerConstants.headingPIDFCoefficients.setCoefficients(1.8,0,0.13,0);
+        FollowerConstants.headingPIDFCoefficients.setCoefficients(2,0,0.1,0);
         FollowerConstants.useSecondaryHeadingPID = false;
         FollowerConstants.secondaryHeadingPIDFCoefficients.setCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
 
-        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.0037,0,0.0000007,0.6,0);
+        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.01,0,0,0.6,0);
         FollowerConstants.useSecondaryDrivePID = false;
         FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
 
-        FollowerConstants.zeroPowerAccelerationMultiplier = 5.7;
+        FollowerConstants.driveKalmanFilterParameters.dataCovariance = 1.5;
+        FollowerConstants.driveKalmanFilterParameters.modelCovariance = 9;
+
+        FollowerConstants.zeroPowerAccelerationMultiplier = 3.6;
         FollowerConstants.centripetalScaling = 0.0003;
 
         FollowerConstants.pathEndTimeoutConstraint = 300;
