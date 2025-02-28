24efe4338a7098e2bce83bb9a592d595d10b3252
vlad <vlad.bintintan@gmail.com>
2025-02-28 12:25:51 +0200

    Lower power for individual motor tests

--- TeamCode/src/main/java/pedroPathing/tuners_tests/localization/MotorDirections.java
+++ TeamCode/src/main/java/pedroPathing/tuners_tests/localization/MotorDirections.java
@@ -1,14 +1,5 @@
 package pedroPathing.tuners_tests.localization;
 
-import static com.pedropathing.follower.FollowerConstants.leftFrontMotorDirection;
-import static com.pedropathing.follower.FollowerConstants.leftFrontMotorName;
-import static com.pedropathing.follower.FollowerConstants.leftRearMotorDirection;
-import static com.pedropathing.follower.FollowerConstants.leftRearMotorName;
-import static com.pedropathing.follower.FollowerConstants.rightFrontMotorDirection;
-import static com.pedropathing.follower.FollowerConstants.rightFrontMotorName;
-import static com.pedropathing.follower.FollowerConstants.rightRearMotorDirection;
-import static com.pedropathing.follower.FollowerConstants.rightRearMotorName;
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.pedropathing.util.Constants;
@@ -17,14 +8,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import pedroPathing.constants.FConstants;
+import pedroPathing.constants.LConstants;
 
 import java.util.Arrays;
 import java.util.List;
 
-import pedroPathing.constants.FConstants;
-import pedroPathing.constants.LConstants;
+import static com.pedropathing.follower.FollowerConstants.*;
 
 @TeleOp(name = "Motor Directions", group = "Teleop Test")
 public class MotorDirections extends OpMode {
@@ -75,22 +66,22 @@ public class MotorDirections extends OpMode {
         rightRear.setDirection(rightRearMotorDirection);
 
         if(gamepad1.a)
-            leftFront.setPower(1);
+            leftFront.setPower(0.2);
         else
             leftFront.setPower(0);
 
         if(gamepad1.y)
-            leftRear.setPower(1);
+            leftRear.setPower(0.2);
         else
             leftRear.setPower(0);
 
         if(gamepad1.b)
-            rightFront.setPower(1);
+            rightFront.setPower(0.2);
         else
             rightFront.setPower(0);
 
         if(gamepad1.x)
-            rightRear.setPower(1);
+            rightRear.setPower(0.2);
         else
             rightRear.setPower(0);
 
