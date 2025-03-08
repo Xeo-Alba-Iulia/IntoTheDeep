4de28c7cf6a8614a85c84c8bf4fa243c4af73461
vlad <vlad.bintintan@gmail.com>
2025-03-08 21:37:20 +0200

    Attempt-uri mai bune la sensori

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/subsystems/intake/Sensor.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/subsystems/intake/Sensor.kt
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.systems.subsystems.intake
 
 import com.qualcomm.hardware.rev.RevColorSensorV3
+import com.qualcomm.robotcore.hardware.AnalogInput
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 
@@ -8,19 +9,23 @@ class Sensor(
     hardwareMap: HardwareMap,
 ) {
     private val sensor: RevColorSensorV3 = hardwareMap.get(RevColorSensorV3::class.java, "sensor_color")
+    private val servoSensor: AnalogInput = hardwareMap.analogInput["servo_sensor"]
 
+    /**
+     * **Trebuie neapărat verificat după delay-ul de închidere la gheară**
+     */
     val isHoldingSample
-        get() = sensor.getDistance(DistanceUnit.CM) < 1.1
+        get() = servoSensor.voltage >= 1.273
 
     val isHoveringSample
-        get() = sensor.getDistance(DistanceUnit.CM) < 3.0
+        get() = sensor.getDistance(DistanceUnit.CM) < 4.0
 
     val isRed
-        get() = sensor.red() > 0.1 && sensor.green() + sensor.blue() < 0.1
+        get() = sensor.red() > 400 && sensor.green() < 300 && sensor.blue() < 200
 
     val isBlue
-        get() = sensor.blue() > 0.1 && sensor.red() + sensor.green() < 0.1
+        get() = sensor.blue() > 400 && sensor.red() < 100
 
     val isYellow
-        get() = sensor.red() > 0.1 && sensor.green() > 0.1 && sensor.blue() < 0.1
+        get() = sensor.red() + sensor.green() > 1000 && sensor.blue() < 500
 }
