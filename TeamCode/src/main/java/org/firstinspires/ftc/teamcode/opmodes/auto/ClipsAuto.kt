0058ef2556b53b35a88be1fe13406197f18e1eb0
vlad <vlad.bintintan@gmail.com>
2025-03-12 12:37:08 +0200

    Update ClipsAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/ClipsAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/ClipsAuto.kt
@@ -4,20 +4,21 @@ import com.acmerobotics.dashboard.FtcDashboard
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket
 import com.pedropathing.follower.Follower
 import com.pedropathing.localization.Pose
-import com.pedropathing.pathgen.PathBuilder
-import com.pedropathing.pathgen.Point
+import com.pedropathing.pathgen.*
 import com.pedropathing.util.Constants
+import com.pedropathing.util.Drawing
 import com.pedropathing.util.Timer
-import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import org.firstinspires.ftc.teamcode.RobotHardware
 import org.firstinspires.ftc.teamcode.systems.IntakePositions
 import org.firstinspires.ftc.teamcode.systems.OuttakePosition
 import org.firstinspires.ftc.teamcode.systems.subsystems.util.Positions
-import org.firstinspires.ftc.teamcode.util.PathFunction
+import org.firstinspires.ftc.teamcode.util.*
 import pedroPathing.constants.FConstants
 import pedroPathing.constants.LConstants
+import kotlin.math.PI
+import kotlin.time.Duration.Companion.seconds
 
 @Autonomous
 class ClipsAuto : LinearOpMode() {
@@ -31,37 +32,39 @@ class ClipsAuto : LinearOpMode() {
     override fun runOpMode() {
         Constants.setConstants(FConstants::class.java, LConstants::class.java)
         val follower = Follower(hardwareMap)
-        val hubList = hardwareMap.getAll(LynxModule::class.java)
 
         val robot = RobotHardware(hardwareMap)
 
         val dashboard = FtcDashboard.getInstance()
-        val angle = Math.PI
-        val beginPoint = Point(9.0, 60.5)
+        val angle = PI
+        val beginPoint = Point(8.0, 60.5)
         val scorePoint =
             arrayOf(
-                Point(38.0, 67.5),
-                Point(38.0, 68.0),
-                Point(38.0, 68.5),
-                Point(38.0, 69.0),
-                Point(38.0, 69.5),
+                Point(37.0, 72.0),
+                Point(36.0, 70.0),
+                Point(37.0, 69.0),
+                Point(37.0, 69.0),
+                Point(37.0, 69.0),
             )
         val samplePoint =
             arrayOf(
                 Point(57.7, 27.0),
                 Point(57.7, 17.0),
-                Point(57.7, 8.9),
-            )
-        val dropPoint =
-            arrayOf(
-                Point(23.0, samplePoint[0].y),
-                Point(23.0, samplePoint[1].y),
-                Point(17.0, samplePoint[2].y),
+                Point(57.7, 10.0),
             )
 
         val scoreControl = Point(7.0, scorePoint[1].y)
-        val pickupPoint = Point(17.0, 30.0)
+        val pickupPoint = Point(8.7, 30.0)
         val parkPoint = Point(22.0, 50.0)
+
+        val dropPoint =
+            arrayOf(
+                Point(30.0, samplePoint[0].y),
+                Point(30.0, samplePoint[1].y),
+                Point(18.0, samplePoint[2].y),
+            )
+
+        val smallScoreControl = Point(26.0, 62.0)
 
         val scorePathList =
             mutableListOf(
@@ -73,19 +76,19 @@ class ClipsAuto : LinearOpMode() {
                         robot.outtake.outtakePosition = OuttakePosition.BAR
                     }.build(),
                 PathBuilder()
-                    .addBezierCurve(dropPoint[2], scoreControl, scorePoint[1])
+                    .addBezierCurve(pickupPoint, scoreControl, scorePoint[1])
                     .setConstantHeadingInterpolation(angle)
                     .addTemporalCallback(0.0) {
                         robot.lift.targetPosition = Positions.Lift.half
                         robot.claw.isClosed = true
-                    }.addTemporalCallback(0.3) { robot.outtake.outtakePosition = OuttakePosition.BAR }
+                    }.addTemporalCallback(0.1) { robot.outtake.outtakePosition = OuttakePosition.BAR }
                     .build(),
             )
 
         scorePathList +=
             Array(3) {
                 PathBuilder()
-                    .addBezierLine(pickupPoint, scorePoint[it + 2])
+                    .addBezierCurve(pickupPoint, smallScoreControl, scorePoint[it + 2])
                     .setConstantHeadingInterpolation(angle)
                     .addTemporalCallback(0.0) {
                         robot.lift.targetPosition = Positions.Lift.half
@@ -97,12 +100,12 @@ class ClipsAuto : LinearOpMode() {
         val returnPath =
             Array(3) {
                 PathBuilder()
-                    .addBezierLine(scorePoint[it + 2], pickupPoint)
+                    .addBezierCurve(scorePoint[it + 2], smallScoreControl, pickupPoint)
                     .setConstantHeadingInterpolation(angle)
                     .addTemporalCallback(0.0) {
                         robot.outtake.pendul.targetPosition = Positions.Pendul.specimenRelease
                         robot.claw.isClosed = false
-                    }.addTemporalCallback(0.3) {
+                    }.addTemporalCallback(0.1) {
                         robot.outtake.outtakePosition = OuttakePosition.PICKUP
                         robot.lift.targetPosition = 0.0
                     }.build()
@@ -113,9 +116,10 @@ class ClipsAuto : LinearOpMode() {
         val goToFirstSample: PathFunction = {
             addBezierCurve(
                 scorePoint[0],
-                Point(12.6, 67.0),
-                Point(24.5, 9.0),
-                Point(66.5, 51.0),
+                Point(13.0, 60.0),
+                Point(23.0, 7.0),
+                Point(60.0, 51.5),
+                Point(64.5, 32.5),
                 samplePoint[0],
             )
             setConstantHeadingInterpolation(angle)
@@ -123,10 +127,14 @@ class ClipsAuto : LinearOpMode() {
                 robot.outtake.pendul.targetPosition = Positions.Pendul.specimenRelease
                 robot.claw.isClosed = false
             }
-            addTemporalCallback(1.0) {
+            addParametricCallback(0.5) {
                 robot.intake.targetPosition = IntakePositions.SPECIMEN_PICKUP
                 robot.outtake.outtakePosition = OuttakePosition.PICKUP
                 robot.lift.targetPosition = 0.0
+                follower.setMaxPower(0.6)
+            }
+            addParametricCallback(0.9) {
+                follower.setMaxPower(1.0)
             }
         }
         val dropFirstSample: PathFunction = {
@@ -135,6 +143,8 @@ class ClipsAuto : LinearOpMode() {
         }
         val goToSecondSample: PathFunction = {
             addBezierCurve(dropPoint[0], Point(67.5, 28.5), samplePoint[1])
+            addParametricCallback(0.4) { follower.setMaxPower(0.6) }
+            addParametricCallback(0.9) { follower.setMaxPower(1.0) }
             setConstantHeadingInterpolation(angle)
         }
         val dropSecondSample: PathFunction = {
@@ -143,12 +153,18 @@ class ClipsAuto : LinearOpMode() {
         }
         val goToThirdSample: PathFunction = {
             addBezierCurve(dropPoint[1], Point(65.0, 16.0), samplePoint[2])
+            addParametricCallback(0.4) { follower.setMaxPower(0.6) }
+            addParametricCallback(0.9) { follower.setMaxPower(1.0) }
             setConstantHeadingInterpolation(angle)
         }
         val dropThirdSample: PathFunction = {
             addBezierLine(samplePoint[2], dropPoint[2])
             setConstantHeadingInterpolation(angle)
         }
+
+        val pickupFirstSpecimen =
+            Path(BezierCurve(dropPoint[2], Point(dropPoint[2].x, pickupPoint.y), pickupPoint))
+        pickupFirstSpecimen.setConstantHeadingInterpolation(PI)
 
         val getSamples =
             PathBuilder()
@@ -183,24 +199,40 @@ class ClipsAuto : LinearOpMode() {
 
         follower.poseUpdater.resetIMU()
         follower.pose = Pose(beginPoint.x, beginPoint.y, angle)
-        state = 0
+        val actions = ActionList<FunctionAction<Int>>()
 
         while (!isStopRequested) {
             when (state) {
                 0 -> {
-                    follower.followPath(scorePath[0])
-                    state = 1
+                    robot.outtake.outtakePosition = OuttakePosition.BAR
+                    robot.lift.targetPosition = Positions.Lift.half
+                    state = -10
+                    actions +=
+                        DelayedAction(0.15.seconds) {
+                            follower.followPath(scorePath[0])
+                            state = 1
+                            1
+                        }
                 }
 
                 1 -> {
                     if (!follower.isBusy) {
-                        follower.followPath(getSamples)
+                        follower.followPath(getSamples, false)
                         state = 2
                     }
                 }
 
                 2 -> {
                     if (!follower.isBusy) {
+                        follower.setMaxPower(0.85)
+                        follower.followPath(pickupFirstSpecimen, true)
+                        state = 21
+                    }
+                }
+
+                21 -> {
+                    if (!follower.isBusy) {
+                        follower.setMaxPower(1.0)
                         follower.followPath(scorePath[1])
                         state = 3
                     }
@@ -259,6 +291,8 @@ class ClipsAuto : LinearOpMode() {
             val packet = TelemetryPacket()
             robot.run(packet)
             dashboard.sendTelemetryPacket(packet)
+            actions()
+            Drawing.drawDebug(follower)
         }
     }
 }
