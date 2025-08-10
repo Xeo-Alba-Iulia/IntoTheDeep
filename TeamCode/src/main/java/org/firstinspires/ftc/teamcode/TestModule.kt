6f29f9e651043c40966cdf600ea9aa56e7656c43
Vlad1-1 <95431727+Vlad1-1@users.noreply.github.com>
2025-08-11 00:42:18 +0300

    Remove hilt for dagger

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestModule.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestModule.kt
@@ -7,8 +7,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(SingletonComponent::class)
 internal object TestModule {
     @Provides
-    fun provideRobot(): RobotHardware = RobotHardware(Any() as HardwareMap)
+    fun provideRobot() = RobotHardware(Any() as HardwareMap)
 }
